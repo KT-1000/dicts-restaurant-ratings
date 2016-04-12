7249a46a9d9e3732386d4de26613af3b339b6d82
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 17:58:11 -0700

    User can enter new restaurants and ratings

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -6,6 +6,9 @@ restaurant_data = argv[1]
 def get_restaurant_ratings(file_name):
 
     restaurant_ratings = {}
+    restaurant_name_input = raw_input('Give me a restaurant name: ')
+    restaurant_rating_input = raw_input('What\'s the rating? ')
+    restaurant_ratings[restaurant_name_input] = restaurant_rating_input
 
     with open(file_name) as in_file:
 
@@ -14,7 +17,7 @@ def get_restaurant_ratings(file_name):
             restaurant_ratings[name] = rating
 
     for name in sorted(restaurant_ratings):
-        print name, "is rated at", restaurant_ratings[name] + "."
+        print name + " is rated at " + str(restaurant_ratings[name]) + "."
 
 
 get_restaurant_ratings(restaurant_data)
